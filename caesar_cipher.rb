7713ba468d9b3860be8bdd31d38840aab5b32dc8
Dzirt232 <dzirt232@rambler.ru>
2017-10-08 17:28:19 +0300

    Caesar cipher is done

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,36 @@
-def caesar_cipher(string)
-	return cipher
+@alphavit = 'abcdefghijklmnopqrstuvwxyz'.split("")
+
+def caesar_cipher(string, key=1)
+	cipher = string.split.map! do |word| 
+		if word == word.capitalize
+			capitalize = true
+		else
+			capitalize = false
+		end
+		word.downcase!
+		curr, word = word, ''
+		curr.each_char do |e|
+			if e.match?(/[a-z]/)
+				index_1 = @alphavit.index(e)
+				if index_1+key > @alphavit.count-1
+					word += @alphavit[key-(@alphavit.count-index_1)] 	
+				else
+				word += @alphavit[index_1+key]
+				end
+			else
+				word += e	
+			end
+		end
+		if capitalize == true
+			word = word.capitalize!
+		else
+			word
+		end
+	end
+	puts cipher.join(" ")
 end
 
-caesar_cipher("Hello!")
+caesar_cipher("Hello!",5)
+caesar_cipher("My name, is Andrey!",3)
+caesar_cipher("How are you?",7)
+caesar_cipher("Whose, who is make mathmatics with me, is very big glassesman.")
