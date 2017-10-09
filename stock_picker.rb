c632c3d556fcc0718a7bef8615c7cc8d2fd3c0aa
Dzirt232 <dzirt232@rambler.ru>
2017-10-09 23:39:30 +0300

    Big progress...

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,8 +1,19 @@
-def stock_picker(days)
 
-	print [days[bay],days[sell]]
+$days_isxodn = [17,5,6,8,2,4,3,7]
+
+def stock_picker(days)
+	bay = days.min
+	sell = days.max
+	if days.index(bay) > days.index(sell)
+		stock_picker(days - [days.delete(sell)])
+	else
+		puts "Bay in the "+ ($days_isxodn.index(bay)).to_s + " day, and sell in " + ($days_isxodn.index(sell).to_s) + " day."
+	end
 end
 
 stock_picker([17,5,6,8,2,4,3,7])
-stock_picker([17,5,6,8,2,5,3,1])
-stock_picker([17,5,6,8,2,5,1,3])
+
+puts "Введите ваши числа: "
+$days_isxodn = gets.chomp.split(" ").map! { |e| e.to_i }
+
+stock_picker($days_isxodn)
