283b2a7886927ab1d363c800aca857a9900b8c81
Dzirt232 <dzirt232@rambler.ru>
2017-10-10 17:22:54 +0300

    Stock Picker is ready!

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,19 +1,24 @@
-
-$days_isxodn = [17,5,6,8,2,4,3,7]
-
 def stock_picker(days)
-	bay = days.min
-	sell = days.max
-	if days.index(bay) > days.index(sell)
-		stock_picker(days - [days.delete(sell)])
-	else
-		puts "Bay in the "+ ($days_isxodn.index(bay)).to_s + " day, and sell in " + ($days_isxodn.index(sell).to_s) + " day."
+	k=1
+	profit=[0]
+	for i in 0..days.count-1 do
+		for j in (i+1)..(days.count-1) do
+			pro=days[j]-days[i]
+			if pro > 0
+				if pro > profit[k-1]
+					profit[k]=pro
+					bay=i
+					sell=j
+					k=k+1
+				end
+			end
+		end
 	end
+	puts "Bay in the "+ bay.to_s + " day, and sell in " + sell.to_s + " day."
 end
 
 stock_picker([17,5,6,8,2,4,3,7])
 
-puts "Введите ваши числа: "
-$days_isxodn = gets.chomp.split(" ").map! { |e| e.to_i }
-
-stock_picker($days_isxodn)
+puts "Введите ваши цены"
+array = gets.chomp.split(" ").map { |e| e.to_i }
+stock_picker(array)
